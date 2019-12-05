0ce03ab9805a231a52798b291d907fa33fc47f6b
resjosh265 <resjosh265@gmail.com>
2019-12-05 16:04:09 -0700

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace zendeskalerts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.email_L = new System.Windows.Forms.Label();
+            this.password_L = new System.Windows.Forms.Label();
+            this.email_val = new System.Windows.Forms.TextBox();
+            this.password_val = new System.Windows.Forms.TextBox();
+            this.remember = new System.Windows.Forms.CheckBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pSound = new System.Windows.Forms.CheckBox();
+            this.menuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearCache = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.zendeskUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // email_L
+            // 
+            this.email_L.AutoSize = true;
+            this.email_L.Location = new System.Drawing.Point(113, 80);
+            this.email_L.Name = "email_L";
+            this.email_L.Size = new System.Drawing.Size(35, 13);
+            this.email_L.TabIndex = 2;
+            this.email_L.Text = "Email:";
+            // 
+            // password_L
+            // 
+            this.password_L.AutoSize = true;
+            this.password_L.Location = new System.Drawing.Point(99, 109);
+            this.password_L.Name = "password_L";
+            this.password_L.Size = new System.Drawing.Size(56, 13);
+            this.password_L.TabIndex = 3;
+            this.password_L.Text = "Password:";
+            // 
+            // email_val
+            // 
+            this.email_val.Location = new System.Drawing.Point(158, 80);
+            this.email_val.Name = "email_val";
+            this.email_val.Size = new System.Drawing.Size(161, 20);
+            this.email_val.TabIndex = 4;
+            // 
+            // password_val
+            // 
+            this.password_val.Location = new System.Drawing.Point(158, 106);
+            this.password_val.Name = "password_val";
+            this.password_val.PasswordChar = '*';
+            this.password_val.Size = new System.Drawing.Size(161, 20);
+            this.password_val.TabIndex = 5;
+            // 
+            // remember
+            // 
+            this.remember.AutoSize = true;
+            this.remember.Location = new System.Drawing.Point(102, 142);
+            this.remember.Name = "remember";
+            this.remember.Size = new System.Drawing.Size(101, 17);
+            this.remember.TabIndex = 7;
+            this.remember.Text = "Remember Me?";
+            this.remember.UseVisualStyleBackColor = true;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pSound
+            // 
+            this.pSound.AutoSize = true;
+            this.pSound.Checked = true;
+            this.pSound.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.pSound.Location = new System.Drawing.Point(233, 142);
+            this.pSound.Name = "pSound";
+            this.pSound.Size = new System.Drawing.Size(86, 17);
+            this.pSound.TabIndex = 8;
+            this.pSound.Text = "Play Sound?";
+            this.pSound.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exit});
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(94, 26);
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(93, 22);
+            this.exit.Text = "exit";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // clearCache
+            // 
+            this.clearCache.Interval = 300000;
+            this.clearCache.Tick += new System.EventHandler(this.clearCache_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // zendeskUrl
+            // 
+            this.zendeskUrl.Location = new System.Drawing.Point(158, 54);
+            this.zendeskUrl.Name = "zendeskUrl";
+            this.zendeskUrl.Size = new System.Drawing.Size(161, 20);
+            this.zendeskUrl.TabIndex = 10;
+            this.zendeskUrl.Text = "https://example.zendesk.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Zendesk URL:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 226);
+            this.Controls.Add(this.zendeskUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pSound);
+            this.Controls.Add(this.remember);
+            this.Controls.Add(this.password_val);
+            this.Controls.Add(this.email_val);
+            this.Controls.Add(this.password_L);
+            this.Controls.Add(this.email_L);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label email_L;
+        private System.Windows.Forms.Label password_L;
+        private System.Windows.Forms.TextBox email_val;
+        private System.Windows.Forms.TextBox password_val;
+        private System.Windows.Forms.CheckBox remember;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.CheckBox pSound;
+        private System.Windows.Forms.ContextMenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+        private System.Windows.Forms.Timer clearCache;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox zendeskUrl;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Media;
+
+namespace zendeskalerts
+{
+    public partial class Form1 : Form
+    {
+        private List<string> _ticketCache = new List<string>();
+        private string _soundLocation = "C:/Windows/Media/notify.wav";
+
+        private NotifyIcon _icon = new NotifyIcon();
+        private StringBuilder _logs = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SystemTraySetup();
+
+            //load the remember me user if available
+            List<string> loadedData = LoadRememberMe();
+
+            //set the values if user is stored in remember me
+            if (string.IsNullOrEmpty(loadedData[0])) return;
+
+            email_val.Text = loadedData[0];
+
+            if (loadedData.Count > 1)
+            {
+                zendeskUrl.Text = loadedData[1];
+            }
+
+            remember.Checked = true;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimerController();
+
+            if (remember.Checked)
+            {
+                //store username in remember me
+                RememberMe();
+            }
+            else
+            {
+                //remove the username from remember me
+                RemoveRememberMe();
+            }
+
+            //start loging
+            _logs.AppendLine(DateTime.Now + ": Started monitoring new tickets.");
+
+            //hide the application to system tray
+            Hide();
+
+            //send a friendly reminder that zenalerts is running in the background
+            Notification("Running", "ZenAlerts is running in the background!");
+        }
+
+        private void SystemTraySetup()
+        {
+            //set system tray icon settings
+            _icon.Visible = true;
+            _icon.Icon = SystemIcons.Application;
+            _icon.ContextMenuStrip = menuStrip;
+        }
+
+        private void TimerController()
+        {
+            //time control
+            timer.Start();
+            clearCache.Start();
+        }
+
+        private void Controller()
+        {
+            FindNewTicket(zendeskUrl.Text, email_val.Text, password_val.Text);
+        }
+
+        private void RememberMe()
+        {
+            List<string> data = new List<string>();
+
+            data.Add(email_val.Text);
+            data.Add(zendeskUrl.Text);
+
+            try
+            {
+                using (var sw = new StreamWriter(Application.StartupPath + "/rememberme.txt"))
+                {
+                    foreach (var row in data)
+                    {
+                        sw.WriteLine(row);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private static void RemoveRememberMe()
+        {
+            try
+            {
+                using (var sw = new StreamWriter(Application.StartupPath + "/rememberme.txt"))
+                {
+                    sw.Write("");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private static List<string> LoadRememberMe()
+        {
+            try
+            {
+                List<string> loadData = new List<string>();
+                using (StreamReader sr = new StreamReader(Application.StartupPath + "/rememberme.txt")) {
+                    while (!sr.EndOfStream)
+                    {
+                        loadData.Add(sr.ReadLine());
+                    }
+                    
+                }
+                return loadData;
+            }
+            catch (Exception)
+            {
+                File.Create(Application.StartupPath + "/rememberme.txt").Close();
+                return null;
+            }
+        }
+
+        private void FindNewTicket(string url, string email, string password)
+        {
+            //logs.AppendLine(System.DateTime.Now.ToString() + ": Checking for new tickets.");
+
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                
+                var req = WebRequest.Create(url + "/api/v2/search.json?query=status:new");
+                var credentials = email + ":" + password;
+                req.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials));
+                var res = req.GetResponse();
+                var receiveStream = res.GetResponseStream();
+                var reader = new StreamReader(receiveStream ?? throw new InvalidOperationException(), Encoding.UTF8);
+                var content = reader.ReadToEnd();
+                Console.WriteLine(content);
+                var json = "[" + content + "]"; // change this to array
+                var objects = JArray.Parse(json); // parse as array  
+                foreach (var o in objects.Children<JObject>())
+                {
+                    foreach (var p in o.Properties())
+                    {
+                        var value = p.Value.ToString().ToLower();
+
+                        if (!value.Contains("new")) continue;
+
+                        //get the ticket ID and check to see if it has been stored in the cache
+                        var ticketId = GetTicketId(value, "\"id\": ", ",");
+                        var isCached = CheckTicketCache(ticketId);
+
+                        if (isCached) continue;
+
+                        _logs.AppendLine(DateTime.Now + ": New Ticket");
+
+                        //play sound and show notification of new ticket
+                        PlaySound();
+                        Notification("New Ticket", "A new ticket has arrived");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Notification("Problem!", "Please check your Zendesk username and password and try again! More details in the log files.");
+                _logs.AppendLine(DateTime.Now + ": " + ex);
+                Show();
+                timer.Stop();
+                clearCache.Stop();
+            }
+        }
+
+        private static string GetTicketId(string strSource, string strStart, string strEnd)
+        {
+            if (!strSource.Contains(strStart) || !strSource.Contains(strEnd)) return string.Empty;
+
+            var start = strSource.IndexOf(strStart, 0, StringComparison.Ordinal) + strStart.Length;
+            var end = strSource.IndexOf(strEnd, start, StringComparison.Ordinal);
+            return strSource.Substring(start, end - start);
+        }
+
+        private bool CheckTicketCache(string id)
+        {
+            if (_ticketCache.Any(t => t == id))
+            {
+                return true;
+            }
+
+            _ticketCache.Add(id);
+            _logs.AppendLine(DateTime.Now + ": Caching ticket #" + id);
+            return false;
+        }
+
+        private void PlaySound()
+        {
+            if (!pSound.Checked) return;
+
+            var sp = new SoundPlayer(_soundLocation);
+            sp.Play();
+        }
+
+        private void Notification(string title, string body)
+        {
+            //show notification
+            _icon.BalloonTipTitle = title;
+            _icon.BalloonTipText = body;
+            _icon.ShowBalloonTip(10000);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            Controller();
+            timer.Start();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            SaveLogs();
+            Application.Exit();
+        }
+
+        private void clearCache_Tick(object sender, EventArgs e)
+        {
+            if (_ticketCache.Count <= 0) return;
+
+            _logs.AppendLine(DateTime.Now + ": dumping the ticket cache.");
+            _ticketCache.Clear();
+        }
+
+        private void SaveLogs()
+        {
+            File.Create(Application.StartupPath + "/logs/logs_" + DateTime.Now.ToString("MM-dd-yyyy hh.mm") + ".txt").Close();
+
+            using (var sw = new StreamWriter(Application.StartupPath + "/logs/logs_" + DateTime.Now.ToString("MM-dd-yyyy hh.mm") + ".txt"))
+            {
+                sw.Write(_logs.ToString());
+            }
+
+            ResetLogs();
+        }
+
+        private void ResetLogs()
+        {
+            _logs.Clear();
+        }
+    }
+}
